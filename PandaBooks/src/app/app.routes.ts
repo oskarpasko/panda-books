c70c1a1b5921214cbd7bf6488759a8825ca1ee25
Oskar <oskarpasko@gmail.com>
2024-11-09 16:34:50 +0100

    create the project

---
+++ PandaBooks/src/app/app.routes.ts
@@ -0,0 +1,8 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { StartwarsComponent } from './startwars/startwars.component';
+
+export const routes: Routes = [
+    {path: '', component: HomeComponent},
+    {path: 'star-wars', component: StartwarsComponent},
+];
